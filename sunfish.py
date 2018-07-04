fc321b744d40c552e46958e35e52d88543a97164
Martin C. Doege <mdoege@compuserve.com>
2018-07-04 12:12:32 +0200

    reduce hash table size

--- sunfish.py
+++ sunfish.py
@@ -109,7 +109,7 @@ MATE_LOWER = piece['K'] - 10*piece['Q']
 MATE_UPPER = piece['K'] + 10*piece['Q']
 
 # The table size is the maximum number of elements in the transposition table.
-TABLE_SIZE = 1e8
+TABLE_SIZE = 1e3
 
 # Constants for tuning search
 QS_LIMIT = 150
